7684dfaf31f1342f444995aaf2c3faf54c752b40
Max van Gorp <534253@student.glu.nl>
2021-07-13 09:19:16 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('login');
+});
+Route::get('login',              'AuthController@index');
+Route::post('post-login',        'AuthController@postLogin');
+Route::get('registration',       'AuthController@registration');
+Route::post('post-registration', 'AuthController@postRegitration');
+Route::get('dashboard',          'AuthController@dashboard');
+Route::get('logout',             'AuthController@logout');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/eigenhtml', function () {
+//     return view('eigenhtml');
+// });
+// // Route::get('/contact', function () {
+// //     return view('contact');
+// // });
+// Route::get('/about', function () {
+//     return view('about');
+// });
+// Route::get('/work', function () {
+//     return view('work');
+// });
+
+// Route::get('/contact/{name?}', 'ContactController@index');
+// Route::post('/contact/send', 'ContactController@ContactRequest');
+
+
+
+// Route::get('/home', 'HomeController@index')->name('home');
